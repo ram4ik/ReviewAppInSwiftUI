b908e7e80a48cf4d22bce2c4b3abafdd2bf4e3eb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-14 16:30:43 +0300

    Initial Commit

---
+++ ReviewAppInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReviewAppInSwiftUI
+//
+//  Created by ramil on 14.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReviewAppInSwiftUI/ReviewAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewAppInSwiftUIApp.swift
+//  ReviewAppInSwiftUI
+//
+//  Created by ramil on 14.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ReviewAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
